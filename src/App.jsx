6f39452c741ff89ce445713e4fee9e268f705bfc
Sanjoli06 <goyalsanjoli036@gmail.com>
2025-12-25 17:34:24 +0530

    [Add]: listing of data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { useState } from "react";
+import { colleges as collegeData } from "./data/colleges";
 
 function App() {
+  
+
+  const filteredColleges = collegeData
+    
+
   return (
-    <div>
-      <h1>Hiiii</h1>
+    <div className="min-h-screen px-6 py-10">
+
+      {/* Header */}
+      <h1 className="text-3xl font-bold text-center mb-8">
+        🎓 College Finder
+      </h1>
+
+     <ul>
+      {filteredColleges.map(college => (
+        <li key={college.id}>
+         {college.name} -- {college.city} | Rating: {college.rating} | Fees: ₹{college.fees}
+        </li>
+      ))}
+     </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
