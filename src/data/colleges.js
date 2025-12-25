1bd31be29ab9c56ea7dfc7c26f8319c6df48d3f9
Sanjoli06 <goyalsanjoli036@gmail.com>
2025-12-26 02:15:20 +0530

    add

--- src/data/colleges.js
+++ src/data/colleges.js
@@ -1,7 +1,13 @@
+import sed from "../assets/8410993.jpg";
+import sed1 from "../assets/Amity_Uniersity_Mumbai.avif";
+import sed2 from "../assets/OIP.jfif";
+import sed3 from "../assets/gib.jfif";
+import sed4 from "../assets/univ.jpg";
+
 export const colleges = [
-  { id: 1, name: "IIT Delhi", city: "New Delhi", rating: 4.8, fees: 200000 },
-  { id: 2, name: "Amity University", city: "Noida", rating: 4.2, fees: 300000 },
-  { id: 3, name: "BITS Pilani", city: "Pilani", rating: 4.9, fees: 450000 },
-  { id: 4, name: "DTU", city: "New Delhi", rating: 4.5, fees: 150000 },
-  { id: 5, name: "Manipal Institute", city: "Manipal", rating: 4.4, fees: 350000 }
+  { id: 1, name: "IIT Delhi", city: "New Delhi", rating: 4.8, fees: 200000,image:sed },
+  { id: 2, name: "Amity University", city: "Noida", rating: 4.2, fees: 300000,image: sed1 },
+  { id: 3, name: "BITS Pilani", city: "Pilani", rating: 4.9, fees: 450000 ,image: sed2},
+  { id: 4, name: "DTU", city: "New Delhi", rating: 4.5, fees: 150000,image: sed3 },
+  { id: 5, name: "Manipal Institute", city: "Manipal", rating: 4.4, fees: 350000,image: sed4 }
 ];
